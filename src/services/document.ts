7a52172720cdd2c1335405a95e40a29b30bf6fca
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2026-01-28 03:10:20 -0500

    document and upload services

--- src/services/document.ts
+++ src/services/document.ts
@@ -2,14 +2,6 @@ import { prisma } from "../db/client";
 import type { Document, DocumentMember, Annotation, DocumentType } from "@prisma/client";
 import { getPresignedUploadUrl, getPresignedDownloadUrl } from "../utils/s3";
 
-
-/**
- * Helpers
- */
-
-const generateS3Key = (ownerId: string, title: string, type: string): string => {
-    return `${ownerId}/${title}-${type}-${Date.now()}`;
-}
 interface CreateDocumentInput {
     ownerId: string;
     title: string;
@@ -18,36 +10,6 @@ interface CreateDocumentInput {
     size: number;
     s3Key: string;
 }
-
-interface UploadDocumentInput {
-    userId: string;
-    filename: string;
-    mimetype: string;
-    size: number;
-}
-
-
-const getUploadUrl = async (data: UploadDocumentInput): Promise<{url: string, s3Key: string}> => {
-    try {
-        const s3Key = generateS3Key(data.userId, data.filename, data.mimetype);
-        const url = await getPresignedUploadUrl(s3Key, data.mimetype);
-        return { url, s3Key }; 
-    } catch (error) {
-        console.error("Error getting presigned upload URL and key:", error);
-        throw new Error("Failed to get presigned upload URL and key");
-    }
-}
-
-const getDownloadUrl = async (s3Key: string): Promise<string> => {
-    try {
-        const url = await getPresignedDownloadUrl(s3Key);
-        return url;
-    } catch (error) {
-        console.error("Error getting presigned download URL:", error);
-        throw new Error("Failed to get presigned download URL");
-    }
-}
-
 
 const createDocument = async (data: CreateDocumentInput): Promise<Document> => {
     try {
@@ -89,6 +51,10 @@ const getUserDocuments = async (userId: string): Promise<Document[]> => {
     }
 }
 
+const documentService = {
+    createDocument,
+    getDocuments,
+    getUserDocuments,
+};
 
-
-export { createDocument };
+export default documentService;
